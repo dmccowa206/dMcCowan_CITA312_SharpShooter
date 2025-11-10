bc857250bf70202b92e0b0d5fde04add5a775128
dmccowan <mccowa206@morrisville.edu>
2025-11-10 15:22:25 -0500

    NavMesh agent created and chasing player

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,24 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        agent.SetDestination(player.transform.position);
+    }
+}
